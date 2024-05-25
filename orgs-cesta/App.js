6b1fd7534f885861ef1f90170f2bc85c20f60f82
Dan Santos <110039343+EngDanSantos@users.noreply.github.com>
2024-05-25 17:21:23 -0300

    .

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,233,229,0) 4%, rgba(193,204,216,1) 96%)',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
