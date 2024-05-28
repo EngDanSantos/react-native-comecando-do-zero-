5cc3ee5e679ca41c01dd180bef2711611fc99c81
Dan Santos <110039343+EngDanSantos@users.noreply.github.com>
2024-05-27 23:43:28 -0300

    .

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar,StyleSheet, Text, SafeAreaView } from 'react-native';
+import Cesta from "./src/telas/cesta"
+import Cat from './src/telas/cats';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <SafeAreaView >
+      <Cesta/>
+      <Cat/>    
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,233,229,0) 4%, rgba(193,204,216,1) 96%)',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/telas/cats.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Text } from "react-native"
+
+const Cat = () =>{
+    const name = "Maru"
+    return <Text> Hello, I'm your cat, {name}!</Text>
+}
+export default Cat;
